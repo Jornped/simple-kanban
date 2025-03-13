0454c82fd293efccd7bbb39d7171a04e6bed29cf
Jornped <local@mail.com>
2025-03-13 20:27:38 +0500

    Tasks save in local storage

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ const cancelButton = addTaskWindow.querySelector(".cancel-task");
 
 let parent = null;
 
+let id = 0;
+
+let taskList = {
+    "Not Started": [
+
+    ],
+    "In progress": [
+
+    ],
+    "Completed": [
+
+    ],
+}
+
+
+
 addButtonsList.forEach(button => {
     button.addEventListener("click", () => {
         addTaskWindow.style.display = "flex";
@@ -22,36 +38,82 @@ saveTaskButton.addEventListener("click", () => {
 
     const taskLabel = addTaskWindow.querySelector(".task-input");
     const taskText = taskLabel.value.trim();
+    const category = parent.dataset.category;
 
     if (taskText === "") {
         taskLabel.classList.add("error");
 
         setTimeout(() => {
             taskLabel.classList.remove("error");
-        }, 400); // Убираем эффект через 400 мс
+        }, 400);
 
-        return; // Не даем добавить пустой таск
+        return;
     }
 
-    const newTask = document.createElement("div");
-    newTask.classList.add("task");
+    taskList[category].push({
+        "id": ++id,
+        "text": taskText,
+    });
 
-    const taskTextElement = document.createElement("p");
-    taskTextElement.textContent = taskText;
+    saveData();
+
+    const newTask = createTaskElement({ text: taskText });
+    parent.appendChild(newTask);
+
+    addTaskWindow.style.display = "none";
+    taskLabel.value = "";
+
+    parent = null;
+});
+
+
+addEventListener("DOMContentLoaded", () => {
+    id = +localStorage.getItem("id") || 0;
+    taskList = JSON.parse(localStorage.getItem("tasks")) || {
+        "Not Started": [],
+        "In Progress": [],
+        "Completed": []
+    };
+    document.querySelectorAll(".kanban-column").forEach(column => {
+        const category = column.dataset.category;
+        column.appendChild(renderTasks(taskList[category]));
+    });
+
+});
+
+
+function saveData() {
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    localStorage.setItem("id", id);
+}
+
+function renderTasks(tasks) {
+    const fragment = document.createDocumentFragment();
+    tasks.forEach(task => {
+        fragment.appendChild(createTaskElement(task));
+    });
+    return fragment;
+}
+
+
+function createTaskElement(task) {
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task");
+
+    const taskText = document.createElement("p");
+    taskText.textContent = task.text;
 
     const editButton = document.createElement("button");
     editButton.classList.add("edit-task-btn");
-    editButton.innerHTML = "&#9999";
+    editButton.innerHTML = "&#9999"; // Карандаш
 
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("delete-task-btn");
     deleteButton.textContent = "X";
 
-    newTask.appendChild(taskTextElement);
-    newTask.appendChild(editButton);
-    newTask.appendChild(deleteButton);
+    taskElement.appendChild(taskText);
+    taskElement.appendChild(editButton);
+    taskElement.appendChild(deleteButton);
 
-    parent.appendChild(newTask);
-    addTaskWindow.style.display = "none";
-    taskLabel.value = "";
-});
+    return taskElement;
+}
